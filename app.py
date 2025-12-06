f3ecd57b5abc738f3ebaf1c0217c324971e05a43
MoYusr <mohammadyusr9@gmail.com>
2025-12-06 16:32:40 +0300

    FileDialog added

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QVBoxLayout, QHBoxLayout, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QFileDialog, QPushButton
+from PyQt5.QtCore import QUrl
 import sys
-import main
+import re
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
 
 class Window(QMainWindow):
     def __init__(self):
@@ -15,14 +18,27 @@ class Window(QMainWindow):
         self.setCentralWidget(cen_widget)
 
         vbox = QVBoxLayout()
-        category = 
-
+        button = QPushButton(self)
+        button.setText("Open")
+        button.clicked.connect(self.addFile)
         grid = QGridLayout()   
         
         hbox = QHBoxLayout()
         hbox.addLayout(vbox)
+        vbox.addWidget(button)
         hbox.addLayout(grid)
         cen_widget.setLayout(hbox)
+
+    def addFile(self):
+        dialog = QFileDialog.getOpenFileUrl(self, "Select PDF File", QUrl(), "PDF (*.pdf)")
+        rawurl = str(dialog[0]).split("(")
+        path = rawurl[1].replace("file:///", "")
+        path = path.replace(")", "")
+        path = path.replace("'", "")
+        file = open(path, 'rb')
+        parser = PDFParser(file)
+        doc = PDFDocument(parser)
+        print(doc.info)
 
     def changeText(self):
         self.label.setText("Label2")
