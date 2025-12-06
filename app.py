8de5da9e5f1e3f22d8e09897f8c82a6c582c11fe
MoYusr <mohammadyusr9@gmail.com>
2025-12-06 19:41:30 +0300

    Scrapped PDF upload and implemented form to add books

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QFileDialog, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, QLabel, QFileDialog, QPushButton, QFormLayout
 from PyQt5.QtCore import QUrl
 import sys
 import re
@@ -19,27 +19,45 @@ class Window(QMainWindow):
         self.setCentralWidget(cen_widget)
 
         vbox = QVBoxLayout()
-        button = QPushButton(self)
-        button.setText("Open")
-        button.clicked.connect(self.addFile)
-        grid = QGridLayout()   
+        #button = QPushButton(self)
+        #button.setText("Open")
+        #button.clicked.connect(self.addFile)
+
+        form = QFormLayout()
+        self.inputs = {
+            "Name": QLineEdit(placeholderText='Name'),
+            "Author": QLineEdit(placeholderText='Author'),
+            "Category": QLineEdit(placeholderText='Category')
+        }
+        for key, val in self.inputs.items():
+            form.addWidget(val)
+        add = QPushButton()
+        add.setText("Add")
+        add.clicked.connect(self.appendData)
+        form.addWidget(add)
         
         hbox = QHBoxLayout()
         hbox.addLayout(vbox)
-        vbox.addWidget(button)
-        hbox.addLayout(grid)
+        hbox.addLayout(form)
         cen_widget.setLayout(hbox)
 
-    def addFile(self):
-        dialog = QFileDialog.getOpenFileUrl(self, "Select PDF File", QUrl(), "PDF (*.pdf)")
-        rawurl = str(dialog[0]).split("(")
-        path = rawurl[1].replace("file:///", "")
-        path = path.replace(")", "")
-        path = path.replace("'", "")
-        file = open(path, 'rb')
-        parser = PDFParser(file)
-        doc = PDFDocument(parser)
-        print(doc.info)
+    def appendData(self):
+        data = {key: val.text() for key, val in self.inputs.items()}
+        name = data['Name']
+        author = data['Author']
+        category = data['Category']
+        main.add_book(name, author, category)
+
+    #def addFile(self):
+    #    dialog = QFileDialog.getOpenFileUrl(self, "Select PDF File", QUrl(), "PDF (*.pdf)")
+    #    rawurl = str(dialog[0]).split("(")
+    #    path = rawurl[1].replace("file:///", "")
+    #    path = path.replace(")", "")
+    #    path = path.replace("'", "")
+    #    file = open(path, 'rb')
+    #    parser = PDFParser(file)
+    #    doc = PDFDocument(parser)
+    #    print(doc.info)
 
     def changeText(self):
         self.label.setText("Label2")
